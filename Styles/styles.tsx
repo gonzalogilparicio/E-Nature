b3387191faebc399879d24624ec4c0c7d53256d7
Gonzalo Gil Paricio <gonzalo.gilparicio@gmail.com>
2023-02-26 20:10:25 -0300

    first commit

---
+++ Styles/styles.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  baseText: {
+    fontSize: 15,
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  rowView: {
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+  },
+});
